0dd7ec1ad78ff24cc8dac8fcf1391cfc440bc785
minhtuan1609 <tuan22000@student.hamk.fi>
2023-01-23 14:16:52 +0200

    Add some animation

--- animation.js
+++ animation.js
@@ -1,26 +1,15 @@
-var $animation_elements = $('.animation-element');
-var $window = $(window);
+$(document).on("scroll", function() {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".tag");
 
-function check_if_in_view() {
-  var window_height = $window.height();
-  var window_top_position = $window.scrollTop();
-  var window_bottom_position = (window_top_position + window_height);
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
 
-  $.each($animation_elements, function() {
-    var $element = $(this);
-    var element_height = $element.outerHeight();
-    var element_top_position = $element.offset().top;
-    var element_bottom_position = (element_top_position + element_height);
-
-    //check to see if this current container is within viewport
-    if ((element_bottom_position >= window_top_position) &&
-      (element_top_position <= window_bottom_position)) {
-      $element.addClass('in-view');
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
     } else {
-      $element.removeClass('in-view');
+      $(tag).removeClass("visible");
     }
-  });
-}
-
-$window.on('scroll resize', check_if_in_view);
-$window.trigger('scroll');
+  }
+});
